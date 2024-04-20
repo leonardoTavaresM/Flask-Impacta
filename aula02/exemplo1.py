4f6d7fb7123036110d26f7b93e0ced76328ffcc5
leonardoTavaesM <leomaltadesa0712@gmail.com>
2024-04-20 15:02:00 -0300

    feat: second lesson

---
+++ aula02/exemplo1.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+cores_frutas = {
+    "morango": "vermelho",
+	  "uva": "roxo",
+    "banana": "amarelo",
+    "abacaxi": "amarelo",
+    "limão": "verde"
+}
+
+@app.route("/")
+def padrao():
+    return jsonify("Hello World")
+
+@app.route("/frutas/<nome_fruta>/cor")
+def frutas(nome_fruta):
+    if nome_fruta in cores_frutas:
+        return cores_frutas[nome_fruta]
+    return "Não sei"
+
+if __name__ == "__main__":
+    app.run(host = 'localhost', port = 5002, debug = True)
